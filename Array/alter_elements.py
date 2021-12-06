72c6cf62e811280588a3cdccf48a16380742523f
Aaron <psxcw4@nottingham.ac.uk>
2021-12-06 02:37:40 +0000

    optimize Q453 recursion solution and add solution for Q665 in alter_elements.py

--- Array/alter_elements.py
+++ Array/alter_elements.py
@@ -24,6 +24,9 @@ class Solution:
     # sum(nums) + cnt * (n-1) = n * x
     # n = len(nums)
     # x = min(nums) + cnt
+
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
     def minMoves(self, nums: List[int]) -> int:
         cnt = sum(nums) - len(nums) * min(nums)
         return cnt
@@ -35,11 +38,9 @@ class Solution:
     def minMoves1(self, nums: List[int]) -> int:
         if len(set(nums)) == 1:
             return self.cnt
-
-        max_value = max(nums)
-        max_value_index = nums.index(max_value)
-
-        new_nums = [value + 1 if idx != max_value_index else value for idx, value in enumerate(nums)]
+        # sort the nums by descending order
+        nums.sort(reverse=True)
+        new_nums = [nums[0]] + [i+1 for i in nums[1:]]
 
         self.cnt += 1
         return self.minMoves1(new_nums)
@@ -52,9 +53,37 @@ class Solution:
     >>> Solution().checkPossibility(nums)
     >>> true
     """
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
     def checkPossibility(self, nums: List[int]) -> bool:
-        pass
+        length_nums = len(nums)
+        cnt = 0
 
+        # prevent index exceed range
+        nums.extend([float("inf"), float("inf")])
+
+        if length_nums <= 2:
+            return True
+        # Two situation will be False
+        for i in range(1, length_nums):
+            if nums[i-1] > nums[i]:
+                cnt += 1
+            elif nums[i+1] < nums[i-1] and nums[i+2] < nums[i]:
+                # (1).2 increase connect by 1 decrease
+                return False
+        return cnt < 2  # (2).More than 2 decrease
+
+    # Time Complexity: O(n)
+    # Space Complexity: O(n)
+    def checkPossibility1(self, nums: List[int]) -> bool:
+        nums_1, nums_2 = nums[:], nums[:]
+        # find the wrong order pair, either modify the first one or second one
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                nums_1[i] = nums[i + 1]
+                nums_2[i + 1] = nums[i]
+                break
+        return nums_1 == sorted(nums_1) or nums_2 == sorted(nums_2)
     """
     283. Move Zeroes
     https://leetcode.com/problems/move-zeroes/
@@ -67,6 +96,8 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        # Time Complexity: O(n)
+        # Space Complexity: O(1)
         # Keep swap the no_zero_value with the first_zero (Two pointer)
         zero_1st_idx = 0
 
