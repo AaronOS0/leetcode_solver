688a28303cb8564d0d2c3bb081cd5382e8d7f893
Aaron <psxcw4@nottingham.ac.uk>
2022-01-09 23:30:26 +0000

    Add functions to delete node from binary tree.

--- Algorithms/Data_Structure/bst_tree.py
+++ Algorithms/Data_Structure/bst_tree.py
@@ -93,12 +93,79 @@ class BST:
             self.post_order(root.rchild)
             print(root.data, end=',')
 
+    # if we need to delete the leaf node
+    def __remove_node_1(self, node):
+        # whether it is root
+        if node.parent is None:
+            self.root = None
+        if node == node.parent.lchild:
+            node.parent.lchild = None
+        else:
+            node.parent.rchild = None
+
+    # obj node only have left child
+    def __remove_node_21(self, node):
+        # whether it is root
+        if node.parent is None:
+            self.root = node.lchild
+            node.lchild.parent = None
+        elif node == node.parent.lchild:
+            node.parent.lchild = node.lchild
+            node.lchild.parent = node.parent
+        else:
+            node.parent.rchild = node.lchild
+            node.lchild.parent = node.parent
+
+    # obj node only have right child
+    def __remove_node_22(self, node):
+        # whether it is root
+        if node.parent is None:
+            self.root = node.rchild
+            node.rchild.parent = None
+        elif node == node.parent.lchild:
+            node.parent.lchild = node.rchild
+            node.rchild.parent = node.parent
+        else:
+            node.parent.rchild = node.rchild
+            node.rchild.parent = node.parent
+
+    def delete(self, val):
+        if self.root:
+            node = self.search_loop(val)
+            if not node:
+                return False
+            # leaf node
+            if not node.lchild and not node.rchild:
+                self.__remove_node_1()
+            # only left node
+            elif not node.rchild:
+                self.__remove_node_21()
+            # only right node
+            elif not node.lchild:
+                self.__remove_node_22()
+            # two children: replace the node with the minimum node of right branch
+            else:
+                min_node = node.rchild
+                while not min_node.lchild:
+                    min_node = min_node.lchild
+                node.data = min_node.data
+                # delete min_node
+                if min_node.rchild:
+                    self.__remove_node_22(min_node)
+                else:
+                    self.__remove_node_1(min_node)
+
 
 tree = BST([4, 6, 7, 9, 2, 1, 3, 5, 8])
+print("Preorder:")
 tree.pre_order(tree.root)
-print(" ")
+print("\nInorder:")
 tree.in_order(tree.root)
-print(" ")
+print("\nPostOrder:")
 tree.post_order(tree.root)
-print(" ")
+print("\nSearch 4:")
 print(tree.search_loop(4).data)
+print("\ndelete 4:")
+tree.delete(4)
+print("\nSearch 4:")
+print(tree.search_loop(4))
